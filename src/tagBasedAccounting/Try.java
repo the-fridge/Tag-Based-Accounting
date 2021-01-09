a94911bb093a859ad9ffbfca94c32e60c8ecc0e5
Jonas H <33072340+the-fridge@users.noreply.github.com>
2021-01-09 23:33:04 +0100

    trying out database connection
    implementing some basic interactions to build uppon and to expand into an command line interface

--- src/tagBasedAccounting/Try.java
+++ src/tagBasedAccounting/Try.java
@@ -1,7 +1,67 @@
 package tagBasedAccounting;
 
+import java.sql.*;
+
 public class Try {
     public static void main(String[] args){
-        System.out.println("try");
+        SQL sql = new SQL();
+        sql.listTables();
     }
+
+
+
+
+    /*public void selectAll(){
+        String sql = "SELECT id, name, typ FROM Tags";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getString("typ"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }*/
+
+
+    private static class SQL{
+
+        private Connection db = null;
+
+        public SQL(){
+            connect();
+        }
+
+        private void connect() {
+            // SQLite connection string
+            String url = "jdbc:sqlite:./data/buchhaltung_test.sqlite3";
+            try {
+                db = DriverManager.getConnection(url);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        
+
+        void listTables(){
+            String sql = "select name from sqlite_master where type = 'table' and name not like 'sqlite_%'";
+            try(Statement stmt  = db.createStatement();
+                ResultSet rs    = stmt.executeQuery(sql)){
+                while (rs.next()) {
+                    System.out.println(rs.getString("name"));
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
 }
